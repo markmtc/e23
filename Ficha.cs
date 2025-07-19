65aa1ff0e420008a64671e49d561cb1cbcd34788
Marco Díaz <mdiaz@gruposascom.es>
2025-07-19 21:13:45 +0200

    Refactorización de la clase Ficha y ajustes en Program.cs
    Se añade el uso de `StructLayout` en `Ficha.cs` para un control más preciso de la disposición de los campos en memoria. Se eliminan propiedades autoimplementadas y se reemplazan por campos explícitos con offsets, incluyendo nuevos campos de tipo `byte` para posiciones adicionales.

--- Ficha.cs
+++ Ficha.cs
@@ -1,19 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace e23
 {
+    [StructLayout(LayoutKind.Explicit)]
     internal class Ficha
     {
-        public int Numero { get; set; }
-        public UInt32 posicion1 { get; set; }
-        public UInt32 posicion2 { get; set; }
-        public UInt32 posicion3 { get; set; }
-        public UInt32 posicion4 { get; set; }
-
-
+        [FieldOffset(0)]
+        public int Numero; 
+        [FieldOffset(1)]
+        public UInt32 posicion1;
+        [FieldOffset(5)]
+        public UInt32 posicion2;
+        [FieldOffset(9)]
+        public UInt32 posicion3;
+        [FieldOffset(13)]
+        public UInt32 posicion4;
+        [FieldOffset(1)]
+        public byte Pos1Arriba;
+        [FieldOffset(2)]
+        public byte Pos1Derecha;
+        [FieldOffset(3)]
+        public byte Pos1bajo;
+        [FieldOffset(4)]
+        public byte Pos1Izquierda;
+        [FieldOffset(5)]
+        public byte Pos2Arriba;
+        [FieldOffset(6)]
+        public byte Pos2Derecha;
+        [FieldOffset(7)]
+        public byte Pos2bajo;
+        [FieldOffset(8)]
+        public byte Pos2Izquierda;
+        [FieldOffset(9)]
+        public byte Pos3Arriba;
+        [FieldOffset(10)]
+        public byte Pos3Derecha;
+        [FieldOffset(11)]
+        public byte Pos3bajo;
+        [FieldOffset(12)]
+        public byte Pos3Izquierda;
+        [FieldOffset(13)]
+        public byte Pos4Arriba;
+        [FieldOffset(14)]
+        public byte Pos4Derecha;
+        [FieldOffset(15)]
+        public byte Pos4bajo;
+        [FieldOffset(16)]
+        public byte Pos4Izquierda;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
-﻿
-
-
-using e23;
+﻿using e23;
 using System.Data;
-
 
 Helper.cargamapa();
 
-
-
+var Mapa = Helper.Mapa;
+var fichas = Helper.Fichas;
 UInt32[,] Tablero = new UInt32[16, 16];
 
 for (int i = 0; i < 16; i++)
@@ -18,3 +14,7 @@ for (int i = 0; i < 16; i++)
         Tablero[i, j] = 0;
     }
 }
+
+
+ Mapa = Helper.Mapa;
+ fichas = Helper.Fichas;
