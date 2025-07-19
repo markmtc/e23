b9671fce0978290f9c2febf1b00b2543261eb743
Marco Díaz <marco.diaz@outlook.com>
2025-07-19 15:39:06 +0200

    Cambio de tipo de matriz a UInt32 en Program.cs
    Se ha reemplazado la matriz de enteros `ints` por una matriz de tipo `UInt32` llamada `Tablero`. Se ha mantenido la inicialización de los elementos a 0 y la llamada a `Helper.cargamapa()` permanece sin cambios.

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@ using e23;
 using System.Data;
 
 
- Helper.cargamapa();
+Helper.cargamapa();
 
 
 
-int[,] ints = new int[16, 16];
+UInt32[,] Tablero = new UInt32[16, 16];
 
 for (int i = 0; i < 16; i++)
 {
     for (int j = 0; j < 16; j++)
     {
-        ints[i, j] = 0;
+        Tablero[i, j] = 0;
     }
-}   
+}
